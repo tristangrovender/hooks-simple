56d973d226785961385212769bab4e10fa70e4f2
Tristan Grovender <therealtristangrovender@gmail.com>
2020-01-21 10:33:38 -0600

    Refactored useEffect

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -4,19 +4,23 @@ import axios from "axios";
 const ResourceList = ({ resource }) => {
     const [resources, setResources] = useState([]);
 
-    const fetchResource = async resource => {
-        const response = await axios.get(
-            `https://jsonplaceholder.typicode.com/${resource}`
-        );
-
-        setResources(response.data);
-    };
-
     useEffect(() => {
-        fetchResource(resource);
+        (async resource => {
+            const response = await axios.get(
+                `https://jsonplaceholder.typicode.com/${resource}`
+            );
+
+            setResources(response.data);
+        })(resource);
     }, [resource]);
 
-    return <div>{resources.length}</div>;
+    return (
+        <ul>
+            {resources.map(record => (
+                <li key={record.id}>{record.title}</li>
+            ))}
+        </ul>
+    );
 };
 
 export default ResourceList;
